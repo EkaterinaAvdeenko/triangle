1b3ab74bad76b3bdfff4e964cd6abb410da83134
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-12-17 20:34:19 +0100

    triangle_AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/triangle/Point.java
+++ src/main/java/com/epam/rd/autotasks/triangle/Point.java
@@ -1,19 +1,20 @@
 package com.epam.rd.autotasks.triangle;
 
 class Point {
-    private double x;
-    private double y;
 
-    public Point(final double x, final double y) {
-        this.x = x;
-        this.y = y;
-    }
+  private final double x;
+  private final double y;
 
-    public double getX() {
-        return x;
-    }
+  public Point(final double x, final double y) {
+    this.x = x;
+    this.y = y;
+  }
 
-    public double getY() {
-        return y;
-    }
+  public double getX() {
+    return x;
+  }
+
+  public double getY() {
+    return y;
+  }
 }
